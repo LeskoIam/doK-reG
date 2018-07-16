eb41ea9ddd88406047c162699537b7c817662216
Lesko <polensek.matevz@gmail.com>
2018-07-16 21:35:31 +0200

    Added to README and made connection string for database clearer to set.

--- README.md
+++ README.md
@@ -13,8 +13,12 @@ This tries to be a clone of [docreg](https://github.com/scott-abernethy/docreg-w
 2. create virtual environment
 3. install requirements.txt
 4. set your download directory ("config.py" -> UPLOAD_FOLDER)
-5. create necessary tables 
-   * in "config.py" set your database connection string
-   * open "app/models.py" and set "go" variable to 1.
-     Yes, I know, it's not how you are supposed to do it, it's staying for now!
+5. create database and necessary tables (diagram [here](https://app.sqldbm.com/SQLServer/Share/d3OpnSOmAtZQWMHDUGgfREGFrngIE8md_DYjF4jNYw0))
+   * a)
+       * crete new database (using pgAdmin for example)
+       * create new user  and set a password for it
+       * change database, password and user in "config.py"
+   * b) 
+       * open "app/models.py" and set "go" variable to 1.
+         (yes, I know, it's not how you are supposed to do it, it's staying for now!)
 6. run "dok_reg_app.py"
--- config.py
+++ config.py
@@ -13,11 +13,20 @@ class DokRegAppConfig(object):
 
     UPLOAD_FOLDER = r"C:\Users\mpolensek\workspace\personal\doK-reG\upload_dir"
 
-    SQLALCHEMY_DATABASE_URI = "postgresql://dokreg:dokreg@localhost:5432/dokreg"
+    db_user = "dokreg"
+    db_password = "dokreg"
+    db_database = "dokreg"
+    SQLALCHEMY_DATABASE_URI = "postgresql://{user}:{password}@localhost:5432/{database}".format(user=db_user,
+                                                                                                password=db_password,
+                                                                                                database=db_database)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
-ALLOWED_EXTENSIONS = {"txt", "pdf", "png",
-                      "jpg", "jpeg", "gif",
-                      "zip", "rar", "xlsx",
-                      "xls", "docx", "doc"}
+ALLOWED_EXTENSIONS = {"txt", "pdf",
+
+                      "png", "jpg", "jpeg",
+                      "gif",
+
+                      "zip", "rar",
+                      "xlsx", "xls",
+                      "docx", "doc"}
